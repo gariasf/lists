09291165153cf260b9277dd5dbf7fcc2811416df
Guillem Arias <guillem.arias@titanos.tv>
2025-12-03 12:11:25 +0100

    add attribution note for original Lists project

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,13 +2,29 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p>
-        Made with care for designers.{' '}
+        Based on the original{' '}
         <a
           href="https://github.com/listsfordesign/Lists"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Contribute on GitHub
+          Lists
+        </a>
+        {' '}project by{' '}
+        <a
+          href="https://twitter.com/hfranco"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hernan Franco
+        </a>
+        {' '}&{' '}
+        <a
+          href="https://twitter.com/mds"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Matt D. Smith
         </a>
       </p>
     </footer>
